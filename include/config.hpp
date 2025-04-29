d8e0af66d37ab114a7770b3f2b46ffc538372816
KairoHenrique <kairohenrique293@gmail.com>
2025-04-29 20:42:24 -0300

    config add

--- include/config.hpp
+++ include/config.hpp
@@ -1,1 +1,10 @@
-// testando heitor
+#ifndef CONFIG_HPP
+#define CONFIG_HPP
+
+// Quantidade de vizinhos mais similares usados na recomendacao – Kairo
+const int K_VIZINHOS = 5;
+
+// Quantidade de filmes recomendados por usuario – Kairo
+const int N_RECOMENDACOES = 10;
+
+#endif

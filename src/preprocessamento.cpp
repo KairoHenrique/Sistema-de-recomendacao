8083984109ba2bcd532ee9cf6b8d0959d34a5275
KairoHenrique <kairohenrique293@gmail.com>
2025-04-29 14:53:48 -0300

    criamento de algumas funcoes basicas-Kairo

--- src/preprocessamento.cpp
+++ src/preprocessamento.cpp
@@ -2,9 +2,8 @@
 #include "utilitarios.hpp"
 #include <unordered_map>
 #include <iostream>
-
+// Função externa para armazenar os dados dos usuários - Kairo
 std::unordered_map<int, std::unordered_map<int, float>> dadosUsuarios;
-
 void carregarDados(const std::string& caminhoArquivo) {
     std::cout << "[Pre-processamento] Lendo: " << caminhoArquivo << std::endl;
     leituraRapidaCSV(caminhoArquivo, dadosUsuarios);

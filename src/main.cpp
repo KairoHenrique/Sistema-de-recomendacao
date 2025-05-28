699d8ab174d9913be7025cc9083c6a7b892192ad
KairoHenrique <kairohenrique293@gmail.com>
2025-05-27 23:57:39 -0300

    att realease 1.0.0

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,26 @@
 #include <iostream>
+#include <chrono> // Para medir tempo
 #include "preprocessamento.hpp"
 #include "recomendador.hpp"
 #include "config.hpp"
 
 int main() {
+    // Início da contagem de tempo
+    auto inicio = std::chrono::high_resolution_clock::now();
+
     // Executa todas as etapas automaticamente – Kairo
     gerarInput("dados/ratings.csv", "dados/input.dat");
     gerarExplore("dados/input.dat", "dados/explore.dat", N_USUARIOS_EXPLORAR);
     carregarDados("dados/input.dat");
     recomendarParaUsuarios("dados/explore.dat", "resultados/output.dat");
-    
 
-    printf("Execucao concluida com sucesso!\n");
+    // Fim da contagem de tempo
+    auto fim = std::chrono::high_resolution_clock::now();
+
+    // Calcula duração
+    std::chrono::duration<double> duracao = fim - inicio;
+
+    std::cout << "Tempo total de execucao: " << duracao.count() << " segundos" << std::endl;
+
     return 0;
 }
